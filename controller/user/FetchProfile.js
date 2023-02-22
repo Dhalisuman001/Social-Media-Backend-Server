69e558b38c75cb8127bfe41d98c63346ba417f77
AryeshSaha <aryeshsaha5302@gmail.com>
2023-02-22 16:24:43 +0530

    initial fetch profile created and route added by Aryesh

---
+++ controller/user/FetchProfile.js
@@ -0,0 +1,19 @@
+const expressAsyncHandler = require("express-async-handler");
+const User = require("../../model/user/UserModel");
+const validId = require("../../utils/isValid");
+
+const FetchProfileCtrl = expressAsyncHandler(async (req, res) => {
+  const userId = req.params.id;
+  validId(userId);
+
+  try {
+    const profile = await User.findById(id);
+    res.json(profile);
+    res.json(user);
+  } catch (error) {
+    res.status(404);
+    throw new Error("Profile does not exist");
+  }
+});
+
+module.exports = FetchProfileCtrl;
