54334f3b7d4a4dc417c6da84597903c59eabf544
Subhadip Mandal <subhadipmandal688@gmail.com>
2023-03-01 10:17:46 +0530

    delete post ctrl bug fixed

--- controller/post/DeletePostCtrl.js
+++ controller/post/DeletePostCtrl.js
@@ -4,20 +4,27 @@ const validId = require("../../utils/isValid");
 
 const DeletePostCtrl = expressAsyncHandler(async (req, res) => {
 
+    const id = req.user._id;
     const PostId = req.body.id;
 
     validId(PostId);
-    
+
     const post = await Post.findById(PostId);
 
-    if (!post) throw new Error("Post does not exist!");
-
-    try {
-        const deletePost = await Post.findByIdAndDelete(PostId);
-        res.json({ message: "Post is deleted", deletedPost: deletePost });
-    } catch (error) {
-        res.json(error);
+    if (!post) throw new Error("Post not found!");
+    
+    if (post.author.toString() === id.toString()) {
+        try {
+            const deletePost = await Post.findByIdAndDelete(PostId);
+            res.json({ message: "Post is deleted", deletedPost: deletePost });
+        } catch (error) {
+            res.json(error);
+        }
     }
+
+    else throw new Error("You can't delete anyone's post!");
+
+
 
 });
 
