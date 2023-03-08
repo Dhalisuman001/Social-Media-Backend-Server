5e25a2349af433814c5e93ab525f1355994678da
suman-dhali <126680783+suman-dhali@users.noreply.github.com>
2023-03-08 20:52:15 +0530

    bug fixed

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ require("dotenv").config();
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`https://localhost:${port}`);
+  console.log(`http://127.0.0.1:${port}`);
 });
