4eaf98faf0f5f53b7add2224d4f3d101584f4a31
suman-dhali <129753047+Alienitysuman@users.noreply.github.com>
2023-04-20 11:50:04 +0530

    Bug fixed

--- controller/post/FeedPostCtrl.js
+++ controller/post/FeedPostCtrl.js
@@ -7,7 +7,7 @@ const FetchAllPostsCtrl = expressAsyncHandler(async (req, res) => {
     const posts = await Post.find();
     const AllId = posts?.map((post) => post._id).reverse();
     if (page > 0) {
-      res.json(AllId.slice((page - 1) * 10, page * 10));
+      res.json(AllId.slice(0, page * 10));
     } else {
       res.json(AllId);
     }
